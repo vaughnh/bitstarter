42d1f94df5840bc688ef2fe9321e579ecb11961b
Vaughn Harmon <vaughn.harmon@gmail.com>
2013-07-15 07:28:06 +0000

    Adding additional grader funtionality to check from a url as well as a file. Adding additional classes to the checks.json

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -37,22 +38,43 @@ var assertFileExists = function(infile) {
 };
 
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    return fs.readFileSync(htmlfile);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var logJson = function(out) {
+    console.log(JSON.stringify(out, null, 4));
+};
+
+var checkHtml = function(htmlstring, checksfile) {
+    $ = cheerio.load(htmlstring);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+
+    logJson(out);
+    //return out;
+};
+
+var checkHtmlUrl = function(url, checksfile) {
+    rest.get(url).on('complete', function(result) {
+        if(result instanceof Error) {
+            console.error("Error retreiving url %s", url);
+            this.retry(5000); // try again in 5 seconds
+        } else {
+            checkHtml(result, checksfile);
+        }
+    });
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    return checkHtml(cheerioHtmlFile(htmlfile), checksfile);
 };
 
 var clone = function(fn) {
@@ -63,12 +85,18 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json')
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if(program.url) {
+        console.log("it is a url");
+        checkHtmlUrl(program.url, program.checks);
+    } else if (program.file) {
+        checkHtmlFile(program.file, program.checks);   
+    }
+    //var checkJson = checkHtmlFile(program.file, program.checks);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
