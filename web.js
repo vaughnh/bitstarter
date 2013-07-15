d7bf148865514aa98fbea50008b8420cf055cb0b
Vaughn Harmon <vaughn.harmon@gmail.com>
2013-07-15 04:27:43 +0000

    Learning to do io here

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content = new Buffer(fs.readFile("index.html"));
+  var content = fs.readFile("index.html");
   response.send(content);
 });
 
